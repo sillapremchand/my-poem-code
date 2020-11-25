21bc51a7fc23fafb48e2f570ce98c4730ed5257b
sillapremchand <73951743+sillapremchand@users.noreply.github.com>
2020-11-25 20:38:08 +0530

    My_poem_code.py

---
+++ My_poem_code.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+poem_part1 = Label(root, text="My mother called me Blue Nile")
+poem_part2 = Label(root, text="I am also named by mother White Nile")
+poem_part3 = Label(root, text="When we grew and grew we asked Oh mother, Oh mother")
+poem_part4 = Label(root, text = "Tell us why did you name us Nile")
+poem_part5 = Label(root, text = "Oh mother")
+                   
+poem_part1.pack()
+poem_part2.pack()
+poem_part3.pack()
+poem_part4.pack()
+poem_part5.pack()
