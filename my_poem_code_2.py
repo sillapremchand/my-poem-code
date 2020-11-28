64297e1dfe357e3eaf6aab6b2d58e8d8027f7ed1
sillapremchand <73951743+sillapremchand@users.noreply.github.com>
2020-11-28 13:20:48 +0530

    my_poem_code_2.py

---
+++ my_poem_code_2.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root = Tk()
+poem_part1 = Label(root, text="I Dimbled and Dimbled")
+poem_part2 = Label(root, text="Where is the peak my lord")
+poem_part3 = Label(root, text="I ploughed and ploughed")
+poem_part4 = Label(root, text="Where is the knowledge treasure, my lord")
+poem_part5 = Label(root, text="I saled and saled")
+poem_part6 = Label(root, text="Where is the island of peace,my lord")
+poem_part7 = Label(root, text="Almighty,bless my nation")
+poem_part8 = Label(root, text="With vision and sweet resulting happiness")
+                   
+poem_part1.pack()
+poem_part2.pack()
+poem_part3.pack()
+poem_part4.pack()
+poem_part5.pack()
+poem_part6.pack()
+poem_part7.pack()
+poem_part8.pack()
